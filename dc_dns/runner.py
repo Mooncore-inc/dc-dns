8a445e31de1585e479fd91d7ae7e2b4100784a75
Fazzyt <kirillovmisha1310@gmail.com>
2026-09-18 16:05:59 +0300

    fix: позже подумаю как сделать без лишних полей

--- dc_dns/runner.py
+++ dc_dns/runner.py
@@ -15,7 +15,7 @@ RECORD_FIELDS = {
     "PTR": ["dname"],
 }
 
-async def run(params) -> dict:
+async def run(config, params) -> dict:
     resolver = aiodns.DNSResolver(nameservers=NAME_SERVERS)
     types = [t.upper() for t in params.record_type]
 
